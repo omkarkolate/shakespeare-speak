37ab5c82ddfb1d76753b88854351b9967cdcd2d1
omkarkolate <53403067+omkarkolate@users.noreply.github.com>
2020-12-21 17:10:31 +0530

    feat: Added Error handling and encodeURI

--- main.js
+++ main.js
@@ -8,13 +8,21 @@ translateBtn.addEventListener("click", () => {
   if (input.value) {
     let text = input.value;
     output.innerText = "";
-    fetch(
+
+    let encodedUri = encodeURI(
       `https://api.funtranslations.com/translate/shakespeare.json?text=${text}`
-    )
+    );
+
+    fetch(encodedUri)
       .then((response) => response.json())
       .then((data) => {
         // console.log(data.contents.translated);
         output.innerText = data.contents.translated;
+      })
+      .catch(error => {
+        console.error(`There has been a problem with your fetch operation for resource "${encodedUri}": ` + error.message)
+        output.style.color = "red";
+        output.innerText = "Error: 404 | Something Went wrong 🙄";
       });
   }
 });
